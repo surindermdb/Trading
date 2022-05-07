488b81eb4ae55688bf8bc8b1177a626652d6aff1
surindermdb <surinderkumar.mdb@gmail.com>
2022-05-07 10:34:14 +0530

    changes for chart zindex set

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const BSCURL='https://bscscan.com';
 //Server config
 
 export const APIURL='http://45.56.105.135:3001/api/';
-export const HOST="http://45.56.105.135";
+export const HOST="http://bloki.cc/";
 
 // export const APIURL='http://109.74.204.241:3001/api/';
 // export const HOST="http://dex.fudxcoin.io";
